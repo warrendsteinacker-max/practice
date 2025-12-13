222217cb9c952272ef3d8fe7cf8659f10065d69e
Warren <warrendsteinacker@gmail.com>
2025-12-12 19:41:46 -0500

    n

--- apply-bind-call-functions-practice.js
+++ apply-bind-call-functions-practice.js
@@ -17,6 +17,36 @@ printf = function (greeting, name) {
 const greetingMessage = greet(printf);
 console.log(greetingMessage); // Output: "Hello, man!"
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 // using bind
 
 // bind method creates a new function that, when called, has its this keyword set to the provided value
+const person = {
+  firstName: 'John',
+  lastName: 'Doe',
+    getFullName: function() {
+    return `${this.firstName} ${this.lastName}`;
+    }
+};
+const getFullName = person.getFullName.bind(person);
+console.log(getFullName()); // Output: "John Doe"      
+
+
+// using call
+
+// call method calls a function with a given this value and arguments provided individually 
+function introduce(city, country) {
+  return `My name is ${this.firstName} ${this.lastName} and I live in ${city}, ${country}.`;
+}
